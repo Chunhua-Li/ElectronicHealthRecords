915223020538083bc71f4686c55bcfcc52c83706
Chunhua-Li <143844841+Chunhua-Li@users.noreply.github.com>
2024-01-13 06:00:20 -0500

    Create MyHealthDataTest2.java

---
+++ MyHealthDataTest2.java
@@ -0,0 +1,4 @@
+ This test class shows errors existing in the method 
+ incorrectCalculateBMI(double weight, double height)
+ method. The method is adapted for calculating BMI 
+ in pounds for weight and in inches for heights.
