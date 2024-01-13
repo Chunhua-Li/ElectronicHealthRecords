2002abe08f1bf388a4b7d4dc5fbf88c1b36eca91
Chunhua-Li <143844841+Chunhua-Li@users.noreply.github.com>
2024-01-13 05:55:54 -0500

    Create MyHealthDataBase.java

---
+++ MyHealthDataBase.java
@@ -0,0 +1,4 @@
+MyHealthDataBase is a base class to be extended for the Electronic
+Health Records (EHR) System,It contains a method to calculate BMI, 
+a method to calculate maximum heart rate, minimum target heart rate
+maximum target heart rate.
